e4a7f89a83fee8283b67ead34ca7da80372409f6
mindy205273 <48311031+mindy205273@users.noreply.github.com>
2019-04-10 00:57:00 +0800

    Update script.js
    更新新增書籍的彈出視窗

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -21,4 +21,30 @@ $(function () {
     loadBookData();
 });
 
+$(document).ready(function () {
+    var myWindow = $("#window"),
+        undo = $("#undo");
+
+    undo.click(function () {
+        myWindow.data("kendoWindow").open();
+        undo.fadeOut();
+    });
+
+    function onClose() {
+        undo.fadeIn();
+    }
+
+    myWindow.kendoWindow({
+        width: "600px",
+        title: "新增書籍",
+        visible: false,
+        actions: [
+            "Pin",
+            "Minimize",
+            "Maximize",
+            "Close"
+        ],
+        close: onClose
+    }).data("kendoWindow").center().open();
+});
 
