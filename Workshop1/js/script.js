91afc5f135575abda1ff5e06f395a168047b8318
mindy205273 <48311031+mindy205273@users.noreply.github.com>
2019-04-11 03:45:06 +0800

    Update script.js
    新增日期選單，車子標示，修改新增書籍視窗的bug

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -21,17 +21,18 @@ $(function () {
     loadBookData();
 });
 
+//新增書籍視窗
 $(document).ready(function () {
     var myWindow = $("#window"),
-        undo = $("#undo");
+        add_book = $("#add_book");
 
-    undo.click(function () {
+    add_book.click(function () {
         myWindow.data("kendoWindow").open();
-        undo.fadeOut();
+        add_book.fadeOut();
     });
 
     function onClose() {
-        undo.fadeIn();
+        add_book.fadeIn();
     }
 
     myWindow.kendoWindow({
@@ -45,6 +46,118 @@ $(document).ready(function () {
             "Close"
         ],
         close: onClose
-    }).data("kendoWindow").center().open();
+    }).data("kendoWindow").center();
+
+
+    $("#grid").kendoGrid({
+        dataSource: {
+            data: bookData,
+            schema: {
+                model: {
+                    fields: {
+                        BookId: { type: "number" },
+                        BookName: { type: "string" },
+                        BookCategory: { type: "string" },
+                        BookAuthor: { type: "string" },
+                        BookBoughtDate: { type: "date" },
+                        BookDeliveredDate: { type: "date" },
+                        BookPrice: { type: "number" },
+                        BookAmount: { type: "number" },
+                        BookTotal: { type: "number" },
+                    }
+                }
+            },
+            pageSize: 20
+        },
+        height: 700,
+        scrollable: true,
+        sortable: true,
+        toolbar: kendo.template($("#search").html()),
+        pageable: {
+            input: true,
+            numeric: false
+        },
+        columns: [
+            {
+                command: "destroy",
+                title: "&nbsp;",
+                width: 120,
+            },
+            {
+                field: "BookId",
+                title: "書籍編號",
+                width: 90,
+            },
+            {
+                field: "BookName",
+                title: "書籍名稱",
+                width: 120,
+            },
+            {
+                field: "BookCategory",
+                title: "書籍種類",
+                values: bookCategoryList,
+                width: 110,
+            },
+            {
+                field: "BookAuthor",
+                title: "作者",
+                width: 120,
+            },
+            {
+                field: "BookBoughtDate",
+                title: "購買日期",
+                format: "{0:yyyy-MM-dd}",
+                width: 140,
+            },
+            {
+                field: "BookDeliveredDate",
+                title: "送達狀態",
+                template: function (item) {
+                    if (item.BookDeliveredDate != undefined) {
+                        return "<i class=\"fas fa-truck\"></i>";
+                    } else {
+                        return "";
+                    }
+                },
+                width: 150,
+            },
+            {
+                field: "BookPrice",
+                title: "金額",
+                format: "{0:n0}",
+                width: 100,
+            },
+            {
+                field: "BookAmount",
+                title: "數量",
+                width: 80,
+            },
+            {
+                field: "BookTotal",
+                title: "總計",
+                format: "{0:n0}元",
+                width: 150,
+            },
+        ]
+    });
+
+    //刪除書籍視窗
+    $("#delete_book_window").kendoWindow({
+        width: "600px",
+        title: "刪除書籍",
+        visible: false,
+        actions: [
+            "Pin",
+            "Minimize",
+            "Maximize",
+            "Close"
+        ],
+        close: onClose
+    }).data("kendoWindow").center();
+
+    $("#bought_datepicker").kendoDatePicker();
+    $("#delivered_datepicker").kendoDatePicker();
+
 });
 
